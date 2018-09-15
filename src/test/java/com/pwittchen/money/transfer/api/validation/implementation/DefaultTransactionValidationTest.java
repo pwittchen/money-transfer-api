7ef6d9210e8baf55eadb30f66f510876d99943f7
Piotr Wittchen <piotr@wittchen.io>
2018-09-15 22:59:49 +0200

    adding placeholder for transaction validation test cases

--- src/test/java/com/pwittchen/money/transfer/api/validation/implementation/DefaultTransactionValidationTest.java
+++ src/test/java/com/pwittchen/money/transfer/api/validation/implementation/DefaultTransactionValidationTest.java
@@ -1,5 +1,31 @@
 package com.pwittchen.money.transfer.api.validation.implementation;
 
+import org.junit.Test;
+
 public class DefaultTransactionValidationTest {
 
+  @Test
+  public void shouldNotGetCommitErrorForValidTransaction() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldGetErrorWhenSenderAccountDoesNotExist() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldGetErrorWhenReceiverAccountDoesNotExist() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldGetErrorWhenSenderBalanceIsLessThanMoneyToBeSend() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldGetErrorWhenTwoAccountsHaveFundsInDifferentCurrencies() {
+    //TODO: implement
+  }
 }
