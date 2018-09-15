ec2040d3a14f62b57396eccc1a107d7ed5a47716
Piotr Wittchen <piotr@wittchen.io>
2018-09-15 19:34:09 +0200

    updating transaction repository

--- src/main/java/com/pwittchen/money/transfer/api/repository/inmemory/InMemoryTransactionRepository.java
+++ src/main/java/com/pwittchen/money/transfer/api/repository/inmemory/InMemoryTransactionRepository.java
@@ -1,6 +1,8 @@
 package com.pwittchen.money.transfer.api.repository.inmemory;
 
+import com.pwittchen.money.transfer.api.model.Account;
 import com.pwittchen.money.transfer.api.model.Transaction;
+import com.pwittchen.money.transfer.api.repository.AccountRepository;
 import com.pwittchen.money.transfer.api.repository.TransactionRepository;
 import com.pwittchen.money.transfer.api.repository.exception.NotEnoughMoneyException;
 import io.reactivex.Completable;
@@ -13,6 +15,11 @@ import org.joda.money.Money;
 public class InMemoryTransactionRepository implements TransactionRepository {
 
   private final Queue<Transaction> transactions = new LinkedList<>();
+  private final AccountRepository accountRepository;
+
+  public InMemoryTransactionRepository(final AccountRepository accountRepository) {
+    this.accountRepository = accountRepository;
+  }
 
   @Override public Completable commit(Transaction transaction) {
     return Completable.create(emitter -> {
@@ -20,14 +27,40 @@ public class InMemoryTransactionRepository implements TransactionRepository {
       Money moneyWithTransactionFee = getMoneyWithTransactionFee(transaction);
 
       if (senderBalance.isGreaterThan(moneyWithTransactionFee)) {
-        //TODO: update balance on the account repository (inject it with DI)
+        //TODO: use DI
         //TODO: consider other validation variants
-        //TODO: consider using validation rate or allow to transfer only the same currency
+        //TODO: consider using conversion rate or allow to transfer only the same currency
+
+        Account senderAccount = updateSenderAccount(transaction, moneyWithTransactionFee);
+        Account receiverAccount = updateReceiverAccount(transaction, transaction.money());
+
+        accountRepository.update(transaction.from().number(), senderAccount);
+        accountRepository.update(transaction.to().number(), receiverAccount);
         transactions.add(transaction);
       } else {
         emitter.onError(new NotEnoughMoneyException(transaction.from().number()));
       }
     });
+  }
+
+  @NotNull
+  private Account updateSenderAccount(Transaction transaction, Money moneyWithTransactionFee) {
+    //TODO: refactor it to builder pattern
+    return new Account(
+        transaction.from().number(),
+        transaction.from().user(),
+        transaction.from().allowedDebit(),
+        transaction.from().money().minus(moneyWithTransactionFee));
+  }
+
+  @NotNull
+  private Account updateReceiverAccount(Transaction transaction, Money money) {
+    //TODO: refactor it to builder pattern
+    return new Account(
+        transaction.from().number(),
+        transaction.from().user(),
+        transaction.from().allowedDebit(),
+        transaction.from().money().plus(money));
   }
 
   @NotNull private Money getMoneyWithTransactionFee(Transaction transaction) {
