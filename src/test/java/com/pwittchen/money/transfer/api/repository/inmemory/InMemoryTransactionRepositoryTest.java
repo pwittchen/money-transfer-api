002a36595c7e5f5a43809c92e98b62c888996e55
Piotr Wittchen <piotr@wittchen.io>
2018-09-15 22:01:10 +0200

    adding placeholders for test cases, which need to be written

---
+++ src/test/java/com/pwittchen/money/transfer/api/repository/inmemory/InMemoryTransactionRepositoryTest.java
@@ -0,0 +1,7 @@
+package com.pwittchen.money.transfer.api.repository.inmemory;
+
+import static org.junit.Assert.*;
+
+public class InMemoryTransactionRepositoryTest {
+
+}
---
+++ src/test/java/com/pwittchen/money/transfer/api/validation/implementation/DefaultTransactionValidationTest.java
@@ -0,0 +1,7 @@
+package com.pwittchen.money.transfer.api.validation.implementation;
+
+import static org.junit.Assert.*;
+
+public class DefaultTransactionValidationTest {
+
+}
