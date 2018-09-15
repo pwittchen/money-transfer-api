f9a01c65c77c0a31df7ce897c11f4f6efe1da1ad
Piotr Wittchen <piotr@wittchen.io>
2018-09-15 09:43:27 +0200

    adding dependency to javalin, slf4J, truth, mockito and shadow gradle plugin

--- src/main/java/com/pwittchen/money/transfer/api/App.java
+++ src/main/java/com/pwittchen/money/transfer/api/App.java
@@ -1,7 +1,17 @@
 package com.pwittchen.money.transfer.api;
 
+import io.javalin.Javalin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App {
+
+  private static final Logger LOG = LoggerFactory.getLogger(App.class);
+  private static final int PORT = 8000;
+
   public static void main(String args[]) {
-    System.out.println("app started...");
+    Javalin app = Javalin.create().start(PORT);
+    app.get("/", context -> context.result("server is running"));
+    LOG.info("server is running");
   }
 }
