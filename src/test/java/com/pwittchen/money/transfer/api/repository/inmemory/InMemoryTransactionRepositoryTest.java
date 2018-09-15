46b25a9c34a0f5885782c7d0e849dfe7b2f4f82a
Piotr Wittchen <piotr@wittchen.io>
2018-09-15 23:07:49 +0200

    adding placeholders for the future tests for transaction repository and transaction validation

--- src/test/java/com/pwittchen/money/transfer/api/repository/inmemory/InMemoryTransactionRepositoryTest.java
+++ src/test/java/com/pwittchen/money/transfer/api/repository/inmemory/InMemoryTransactionRepositoryTest.java
@@ -1,5 +1,48 @@
 package com.pwittchen.money.transfer.api.repository.inmemory;
 
+import com.pwittchen.money.transfer.api.repository.AccountRepository;
+import com.pwittchen.money.transfer.api.repository.TransactionRepository;
+import com.pwittchen.money.transfer.api.validation.TransactionValidation;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
 public class InMemoryTransactionRepositoryTest {
 
+  private TransactionRepository transactionRepository;
+
+  @Mock
+  private AccountRepository accountRepository;
+
+  @Mock
+  private TransactionValidation transactionValidation;
+
+  @Before
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+    this.transactionRepository = new InMemoryTransactionRepository(
+        accountRepository, transactionValidation
+    );
+  }
+
+  @Test
+  public void shouldNotCommitTransactionWhenValidationDetectedError() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldCommitTransaction() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldGetCreatedTransaction() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldGetAllTransactions() {
+    //TODO: implement
+  }
 }
--- src/test/java/com/pwittchen/money/transfer/api/validation/implementation/DefaultTransactionValidationTest.java
+++ src/test/java/com/pwittchen/money/transfer/api/validation/implementation/DefaultTransactionValidationTest.java
@@ -1,8 +1,24 @@
 package com.pwittchen.money.transfer.api.validation.implementation;
 
+import com.pwittchen.money.transfer.api.repository.AccountRepository;
+import com.pwittchen.money.transfer.api.validation.TransactionValidation;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 public class DefaultTransactionValidationTest {
+
+  private TransactionValidation transactionValidation;
+
+  @Mock
+  private AccountRepository accountRepository;
+
+  @Before
+  public void setUp() {
+    MockitoAnnotations.initMocks(this);
+    this.transactionValidation = new DefaultTransactionValidation(accountRepository);
+  }
 
   @Test
   public void shouldNotGetCommitErrorForValidTransaction() {
