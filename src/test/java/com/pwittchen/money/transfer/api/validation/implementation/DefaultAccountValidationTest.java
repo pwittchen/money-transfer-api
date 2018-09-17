5663a014535328b37bf8560e7d16f866470a0811
Piotr Wittchen <piotr@wittchen.io>
2018-09-17 15:50:58 +0200

    adding placeholders for AccountValidation tests

--- src/test/java/com/pwittchen/money/transfer/api/validation/implementation/DefaultAccountValidationTest.java
+++ src/test/java/com/pwittchen/money/transfer/api/validation/implementation/DefaultAccountValidationTest.java
@@ -1,5 +1,54 @@
 package com.pwittchen.money.transfer.api.validation.implementation;
 
+import com.pwittchen.money.transfer.api.validation.AccountValidation;
+import org.junit.Test;
+
 public class DefaultAccountValidationTest {
-  //TODO: implement
+
+  private AccountValidation accountValidation = new DefaultAccountValidation();
+
+  @Test
+  public void shouldNotCreateAnyErrorsIfAccountIsValid() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldCreateErrorIfAccountNumberIsNull() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldCreateErrorIfAccountNumberIsEmpty() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldCreateErrorIfUserIdIsNull() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldCreateErrorIfUserIdIsEmpty() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldCreateErrorIfUserNameIsNull() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldCreateErrorIfUserNameIsEmpty() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldCreateErrorIfUserSurnameIsNull() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldCreateErrorIfUserSurnameIsEmpty() {
+    //TODO: implement
+  }
 }
