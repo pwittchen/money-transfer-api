3e6359d61c005ba124b8baa0f51d357a925db574
Piotr Wittchen <piotr@wittchen.io>
2018-09-17 15:47:44 +0200

    adding placeholders for TransactionControllerTest

--- src/test/java/com/pwittchen/money/transfer/api/controller/TransactionControllerTest.java
+++ src/test/java/com/pwittchen/money/transfer/api/controller/TransactionControllerTest.java
@@ -4,6 +4,7 @@ import com.pwittchen.money.transfer.api.controller.context.ContextWrapper;
 import com.pwittchen.money.transfer.api.repository.AccountRepository;
 import com.pwittchen.money.transfer.api.repository.TransactionRepository;
 import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mock;
 
 public class TransactionControllerTest {
@@ -24,5 +25,40 @@ public class TransactionControllerTest {
     this.controller = new TransactionController(
         contextWrapper, transactionRepository, accountRepository
     );
+  }
+
+  @Test
+  public void shouldGetOneTransaction() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldGetAllTransactions() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldCommitTransaction() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldNotCommitTransactionIfSenderIsNotPresent() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldNotCommitTransactionIfReceiverIsNotPresent() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldNotCommitTransactionIfMoneyHasInvalidFormat() {
+    //TODO: implement
+  }
+
+  @Test
+  public void shouldNotCommitTransactionIfValidationErrorOccurred() {
+    //TODO: implement
   }
 }
